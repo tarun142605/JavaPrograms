32dd88e9d6061a5cce462f2c75b3c83d0351796d
tarun142605 <tarun142605@gmail.com>
2023-11-08 08:49:24 +0530

    First commit

---
+++ Q1.java
@@ -0,0 +1,13 @@
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("This is the main method with String[] args parameter.");
+    }
+
+    public static void main(String arg) {
+        System.out.println("This is the main method with String arg parameter.");
+    }
+
+    public static void main(int arg) {
+        System.out.println("This is the main method with int arg parameter.");
+    }
+}
